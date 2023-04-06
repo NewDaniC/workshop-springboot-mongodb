fa9e90035119a252deb6ccfac1663535a4052446
NewDaniC <danieltheisgesdacosta@gmail.com>
2023-04-06 00:21:05 -0300

    Obtendo um post por id

---
+++ src/main/java/com/danielcosta/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.danielcosta.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.danielcosta.workshopmongo.domain.Post;
+import com.danielcosta.workshopmongo.repository.PostRepository;
+import com.danielcosta.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
