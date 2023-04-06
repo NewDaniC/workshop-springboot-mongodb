6dbdf72fd2bd9182b4ecc3c571e949d4e684bccc
NewDaniC <danieltheisgesdacosta@gmail.com>
2023-04-05 22:36:54 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/danielcosta/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielcosta.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
